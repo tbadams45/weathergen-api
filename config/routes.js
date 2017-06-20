9c1d1fa58bfbc1568a83552fdd89062293caa30c
Timothy Adams <tbadams45@gmail.com>
2017-06-20 13:28:07 -0400

    can recieve JSON data at api/wgen/single

--- config/routes.js
+++ config/routes.js
@@ -2,6 +2,6 @@
  * Created by tbadams45 on 6/20/17.
  */
 module.exports = function(app) {
-    //app.use('/api', require('../routes/api'));
+    app.use('/api', require('../routes/api'));
     app.use('/', require('../routes/index'));
 }
--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,25 @@
 /**
  * Created by tbadams45 on 6/20/17.
  */
+var express = require('express');
+var router = express.Router();
+var uuid = require('uuid');
+var path = require('path');
+var fs = require('fs');
+var kue = require('kue');
+var env = process.env.NODE_ENV || 'development',
+    config = require('../config/config')[env];
+
+var jobs = kue.createQueue();
+
+// req.body.data, req.body.inputs
+router.post('/wgen/single', function(req, res) {
+    console.log(req.body.data);
+    console.log(req.body.inputs);
+});
+
+router.post('/wgen/batch', function(req, res) {
+
+});
+
+module.exports = router;
